0a5583700732fe31f6e1f608f9f6063978d7f95c
shinharad <shinharad.github@gmail.com>
2020-01-05 00:07:08 +0900

    add CartRoutes

--- modules/adapter/src/main/scala/shop/adapter/codec/UnderlyingCodecs.scala
+++ modules/adapter/src/main/scala/shop/adapter/codec/UnderlyingCodecs.scala
@@ -4,8 +4,12 @@ import io.circe._
 import io.circe.generic.semiauto._
 import shop.domain.Brands._
 import shop.domain.Categories.Category
+//import shop.domain.Checkout.Card
 import shop.domain.Items.Item
+import shop.domain.Orders.Order
+import shop.domain.ShoppingCart._
 import shop.domain.Users.User
+//import shop.infrastructure.refined._
 import squants.market._
 
 trait UnderlyingCodecs extends NewtypeCodecs {
@@ -23,18 +27,18 @@ trait UnderlyingCodecs extends NewtypeCodecs {
   implicit val itemDecoder: Decoder[Item] = deriveDecoder[Item]
   implicit val itemEncoder: Encoder[Item] = deriveEncoder[Item]
 
-  //  implicit val cartItemDecoder: Decoder[CartItem] = deriveDecoder[CartItem]
-  //  implicit val cartItemEncoder: Encoder[CartItem] = deriveEncoder[CartItem]
-  //
-  //  implicit val cartTotalEncoder: Encoder[CartTotal] = deriveEncoder[CartTotal]
-  //
-  //  implicit val orderEncoder: Encoder[Order] = deriveEncoder[Order]
-  //
-  //  implicit val cardDecoder: Decoder[Card] = deriveDecoder[Card]
+  implicit val cartItemDecoder: Decoder[CartItem] = deriveDecoder[CartItem]
+  implicit val cartItemEncoder: Encoder[CartItem] = deriveEncoder[CartItem]
 
-  //  implicit val cartEncoder: Encoder[Cart] = Encoder.forProduct1("items")(_.items)
-  //  implicit val cartDecoder: Decoder[Cart] = Decoder.forProduct1("items")(Cart.apply)
-  //
+  implicit val cartTotalEncoder: Encoder[CartTotal] = deriveEncoder[CartTotal]
+
+  implicit val orderEncoder: Encoder[Order] = deriveEncoder[Order]
+
+//  implicit val cardDecoder: Decoder[Card] = deriveDecoder[Card]
+
+  implicit val cartEncoder: Encoder[Cart] = Encoder.forProduct1("items")(_.items)
+  implicit val cartDecoder: Decoder[Cart] = Decoder.forProduct1("items")(Cart.apply)
+
   implicit val userDecoder: Decoder[User] = deriveDecoder[User]
   implicit val userEncoder: Encoder[User] = deriveEncoder[User]
 
---
+++ modules/adapter/src/main/scala/shop/adapter/http/routes/secured/CartRoutes.scala
@@ -0,0 +1,65 @@
+package shop.adapter.http.routes.secured
+
+import cats.effect.Sync
+import org.http4s._
+import org.http4s.dsl.Http4sDsl
+import shop.domain.Items.ItemId
+import shop.domain.ShoppingCart
+import shop.domain.ShoppingCart.Cart
+import shop.domain.Users.CommonUser
+//import shop.algebras.ShoppingCart
+//import shop.domain.cart.Cart
+//import shop.http.auth.users.CommonUser
+//import shop.http.json._
+import cats.instances.list._
+import cats.syntax.traverse._
+import cats.syntax.apply._
+import org.http4s.server._
+import shop.adapter.http.codecs._
+//import shop.domain.item.ItemId
+
+final class CartRoutes[F[_]: Sync](
+    shoppingCart: ShoppingCart[F]
+) extends Http4sDsl[F] {
+
+  private[routes] val prefixPath = "/cart"
+
+  private val httpRoutes: AuthedRoutes[CommonUser, F] =
+    AuthedRoutes.of {
+      // Get shoppping cart
+      case GET -> Root as user =>
+        Ok(shoppingCart.get(user.value.id))
+
+      // Add items to the cart
+      case ar @ POST -> Root as user =>
+        ar.req.decode[Cart] { cart =>
+          cart.items
+            .map {
+              case (id, quantity) =>
+                shoppingCart.add(user.value.id, id, quantity)
+            }
+            .toList
+            .sequence *> Created()
+        }
+
+      // Modify items in the cart
+      case ar @ PUT -> Root as user =>
+        ar.req.decode[Cart] { cart =>
+          shoppingCart.update(user.value.id, cart) *> Ok()
+        }
+
+      // Remove item from the cart
+      case DELETE -> Root / UUIDVar(uuid) as user =>
+        shoppingCart.removeItem(
+          user.value.id,
+          ItemId(uuid)
+        ) *> NoContent()
+    }
+
+  def routes(
+      authMiddleware: AuthMiddleware[F, CommonUser]
+  ): HttpRoutes[F] = Router(
+    prefixPath -> authMiddleware(httpRoutes)
+  )
+
+}
